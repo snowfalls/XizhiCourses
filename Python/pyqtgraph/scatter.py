67fcc63df12e7cda5bca4f433bfa16cb77e906fe
bochaodang <272708269@qq.com>
2021-03-30 15:19:19 +0800

    1. modify the scatter graph to use the white background

--- Python/pyqtgraph/scatter.py
+++ Python/pyqtgraph/scatter.py
@@ -1,6 +1,7 @@
 import pyqtgraph as pg
 import numpy as np
+pg.setConfigOption('background', 'w')
 x=np.random.normal(size=100)
 y=np.random.normal(size=100)
-pg.plot(x,y,pen=None,symbol='o')
+pg.plot(x,y,symbol='o')
 input()
