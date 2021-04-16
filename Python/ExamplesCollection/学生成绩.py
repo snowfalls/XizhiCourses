5ea988083c41949dda8f49ca80209ae63dd88ebf
bochaodang <272708269@qq.com>
2021-04-16 10:21:07 +0800

    add 2 handwirte examples 1. 搬金块 2. 学生成绩

--- Python/ExamplesCollection/学生成绩.py
+++ Python/ExamplesCollection/学生成绩.py
@@ -1,3 +1,6 @@
+# 1.某学校的期末考试结束，并统计出了现在的所有同学的成绩现在需要对学生成绩进行分级，通过python编程实现, 
+# 将学生的成绩分类为A、B、C，分别对应高于80,60~80之间的成绩，以及60分以下的成绩。并打印出60分以下成绩同学对应的索引。
+
 import numpy as np 
 
 student_mark = np.random.randint(50,100,size=100) #generate the random number from 50~100 size=100
