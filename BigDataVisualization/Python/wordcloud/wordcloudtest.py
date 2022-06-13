9a498e24b836385b01bde24b716adf3b725dbfb6
bochao <bochao.dang@qq.com>
2022-06-13 11:32:43 +0800

    1. add some examples and delete plenty of repeated files

--- BigDataVisualization/Python/wordcloud/wordcloudtest.py
+++ BigDataVisualization/Python/wordcloud/wordcloudtest.py
@@ -1,12 +1,14 @@
 from wordcloud import WordCloud
 import jieba
 
-with open('2.txt', 'r', encoding='utf-8') as f:
-    text = f.read()
+# with open('2.txt', 'r', encoding='utf-8') as f:
+#     text = f.read()
+
+text = "测试"
 
 cut_text = " ".join(jieba.cut(text))
 cloud = WordCloud(
-    # font_path = "d:\粗活意简.ttf", if the file doesn't exist, just use the english words in docx
+    font_path = "d:\粗活意简.ttf", #if the file doesn't exist, just use the english words in docx
     # if use the chinese words in docx, the ttf file and the line should be executed.
     background_color = "white",
     max_words = 4000,
